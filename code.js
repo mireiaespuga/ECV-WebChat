a346c5f3f1e6b5c32ccd21b523a622cff4939f1a
mireiaespuga <m.espugalopez@gmail.com>
2021-01-20 16:02:43 +0100

    feat: css, html and main js

--- code.js
+++ code.js
@@ -1,151 +1,116 @@
-//import * as utils from './utils.js'
+var myself;
+var server = new SillyClient(); //create our class
+var chat = new Chat();
+var room = new Room();
+var loadbutton;
 
-class Conversation {
-    chat = null
+if(!myself){
+    hideElement('webchat');
+}
+
+server.on_message = function(user_id, data){
+    var message = JSONtoMessage(data)
+
+    switch (message.type) {
+        case types.USER:
+            var newUser = new User(message.userName, user_id);
+            addUserToRoom(room, newUser);
+            break;
+        
+        case types.LOG:
+            room.chats[0].messages = message.text;
+            canShowLog(room, room.name);
+            loadbutton = document.querySelector("#load_button");
+            if(loadbutton){
+                loadbutton.addEventListener("click", function(e){
+                    loadMessages(room);
+                });
+            }   
+            break;
+
+        default:
+            showMessage(room, message, RECEIVE, user_id);
+            addMessage(room.chats[0], message, user_id);
+            break;
+    }
+    
+}
+
+server.on_ready = function(author_id) {
+    myself.id = server.user_id || author_id;
+    addUserToRoom(room, myself);
+    sendMessage(room, types.USER);
+    loadWebchat(myself);
+    hideElement('init');
+    showElement('webchat');
+}
+
+server.on_connect = function() {
+    
+}
+
+server.on_user_connected = function(user_id, data){
+    sendMessage(room, types.USER);
+
+    if(Object.keys(server.clients).map(Number).sort()[0] == myself.id){
+        sendMessage(room, types.LOG, user_id);
+    }
 
 }
 
-class Chat {
-    user = null
-    startDate = null 
-    text = 'this is a preview text that is very interesting once you read it'
+server.on_user_disconnected = function(user_id){  
+	console.log("Somebody has disconnected from the room");
 }
 
-class User {
-    userName = null
-    id = null
-    pic =  null
+server.on_close = function(){  
+	console.log("Server closed the connection" );
 }
 
 
-var midnight = new Date();
-midnight.setHours(24,0,0,0); 
-
-var now = new Date();
-var msToMidnight = midnight - now;
-
+var msToMidnight = msToMidnight();
 midnightTask();
 
-var conversations = []
 
-for (let index = 0; index < 5; index++) {
-    var user = new User() ;
-    user.userName = "Friend";
-    var chat = new Chat();
-    chat.user = user;
-    chat.startDate = new Date();
-
-    conversations.push(chat)
+var submitbutton = document.querySelector("#submit_button");
+submitbutton.addEventListener("click", function(){
+    var roomName = document.getElementById("room").value;
+    room.name = roomName;
+    chat.chatName = roomName;
+    chat.pic = ROOM_AVATAR;
+    room.chats = [chat];
+    var out = validateUser(room)
+    if(out.result){
+        server.connect("wss://ecv-etic.upf.edu/node/9000/ws", room.name.toUpperCase(), function(){  
+            console.log("Connected to server! :)");  
+            myself = new User(out.userName)
+        }
+        );
+    }
 }
+);
 
-showConversations();
+var sendbutton = document.querySelector("#send_button");
+sendbutton.addEventListener("click", function(e){
+    sendMessage(room);
+});
 
+var myavatar = document.querySelector("#my-avatar");
+myavatar.addEventListener("click", function(e){
+   changeAvatar();
+});
 
-var b = document.querySelector("#send_button");
-b.addEventListener("click", sendMessage);
-
-var c = document.querySelector("#conver");
-c.addEventListener("click", loadMessages);
-
+var input = document.querySelector("#type-section")
+input.addEventListener('keydown', function(e){
+    if(e.key == 'Enter'){
+        sendMessage(room);
+    } 
+});
 
 setTimeout(
     midnightTask,
     msToMidnight
 );
  
-function midnightTask() {
-    var cont = document.createElement("div");
-    cont.className = 'date-row';
-
-    var dayDate =  document.createElement("div");
-    dayDate.className = "day-date";
-    dayDate.innerText = new Date(Date.now()).toLocaleString().split(',')[0]; //'d MMM yyyy'
-    var chat = document.querySelector("#chat");
-    cont.appendChild(dayDate);
-    chat.appendChild(cont);
-}
-
-function loadMessages(){
-    
-}
-
-function showConversations(){
-    conversations.forEach( c => {
-        var conv = document.querySelector("#conversations");
-
-        var cont = document.createElement("div");
-        cont.className = 'conv-row';
-        cont.id = "conver"
-
-        var avatar = document.createElement("div");
-        avatar.className = 'avatar';
-        var pic = document.createElement("img");
-        pic.src = c.user.pic || "avatar.png";
-        avatar.appendChild(pic);
-
-        var convInfo = document.createElement("div");
-        convInfo.className = 'contact-info';
-        
-        var userName = document.createElement("div");
-        userName.className = 'user-name';
-        userName.innerText = c.user.userName;
-        
-        
-        var preview = document.createElement("div");
-        preview.className = 'text-preview';
-        preview.innerText = limitLength(c.text, 50);
-        
-        convInfo.appendChild(userName);
-        convInfo.appendChild(preview);
-
-    
-        cont.appendChild(avatar);
-        cont.appendChild(convInfo);
-
-        conv.appendChild(cont);
-    })
-}
-
-function sendMessage(){
-
-    var input = document.querySelector("input");
-    if(input.value == '') return
-
-    var cont = document.createElement("div");
-    cont.className = 'message-row';
-    var bubble = document.createElement("div");
-    bubble.className = 'out-message';
-    var text = document.createElement("p");
-    text.className = 'text-message';
-    
-    text.innerText = input.value;
-    
-    var date = document.createElement("p");
-    date.className = 'date-message';
-    date.innerText = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-
-    bubble.appendChild(text);
-    bubble.appendChild(date);
-    cont.appendChild(bubble);
-
-    var chat = document.querySelector("#chat");
-    chat.appendChild(cont);
-    input.value = '';
-
-    updateScroll();
-}
 
 
-function updateScroll(){
-    var element = document.getElementById("chat");
-    element.scrollTop = element.scrollHeight;
-}
 
-function limitLength(text, maxLength) {
-    if (!text) {
-      return ''
-    }
-    return text.length > maxLength
-      ? text.slice(0, maxLength - 3).concat('...')
-      : text
-  }
